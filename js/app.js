7ca47c1806516e751459fa5d05c2aa1902431fd9
Matthieu Bourgois <matthieubourgois@Matthieus-MacBook-Pro.local>
2018-09-27 12:59:46 -0700

    fixed word print functiton

--- js/app.js
+++ js/app.js
@@ -1,49 +1,68 @@
-/*----- cached element references -----*/
-var wordTable = document.querySelectorAll('td');
-var eggs = document.querySelectorAll('img');
-
-var blankSpaces = document.querySelector('ul');
-var answerList = document.querySelectorAll('li');
-var fiveLives = document.querySelectorAll('.eggs');
-var levelDisplay = document.getElementById('levelDisplay');
-
-var hintBtn = document.getElementById('hint');  
-var retryBtn = document.getElementById('retry');
-hintBtn.addEventListener("click", hint);
-retryBtn.addEventListener("click", retryLevel);
-
-var t1 = document.getElementById('t1');
-var t2 = document.getElementById('t2');
-var t3 = document.getElementById('t3');
-var t4 = document.getElementById('t4');
-var t5 = document.getElementById('t5');
-var t6 = document.getElementById('t6');
-var t7 = document.getElementById('t7');
-var t8 = document.getElementById('t8');
-var t9 = document.getElementById('t9');
-var l1 = document.getElementById('l1');
-var l2 = document.getElementById('l2');
-var l3 = document.getElementById('l3');
-var l4 = document.getElementById('l4');
-var l5 = document.getElementById('l5');
-var l6 = document.getElementById('l6');
-var l7 = document.getElementById('l7');
-
 /*----- functions -----*/ 
+
 function initGame(){
-    lives = 5;
+    addClickListener()
+    answerDisplay();
+    lives.length = 5;
+    joinedWordsSeperate.length = 0;
+    lettersPlayed.length = 0;
+    gameWon = false;
+    gameOver = false;
+    lifeLoss = false;
+    levelDisplay.textContent = "Level: " + (level + 1);
+    lifeCreator();
+
+    for(let cube of wordTable){
+        cube.style.borderColor = "black";
+    }
+    t1.textContent = levelsArray[level].board[0];
+    t2.textContent = levelsArray[level].board[1];
+    t3.textContent = levelsArray[level].board[2];
+    t4.textContent = levelsArray[level].board[3];
+    t5.textContent = levelsArray[level].board[4];
+    t6.textContent = levelsArray[level].board[5];
+    t7.textContent = levelsArray[level].board[6];
+    t8.textContent = levelsArray[level].board[7];
+    t9.textContent = levelsArray[level].board[8];
+
+    l1.textContent = levelsArray[level].board[9];
+    l2.textContent = levelsArray[level].board[10];
+    l3.textContent = levelsArray[level].board[11];
+    l4.textContent = levelsArray[level].board[12];
+    l5.textContent = levelsArray[level].board[13];
+    l6.textContent = levelsArray[level].board[14];
+    l7.textContent = levelsArray[level].board[15];
+
+    gameOverScreen();
+
+}
+initGame();
+
+
+//*--- FUNCTIONS ---*//
+// initiates next level, changes board to next level
+function nextLevel(){
+    if(level>=7){
+        levelUp();
+    }
+    level+=1;
+    lettersPlayed.length = 0;
+    joinedWordsSeperate.length = 0;
+    addClickListener()
+    answerDisplay();
+
+    lives.length = 5;
     joinedWordsSeperate = [];
     lettersPlayed = [];
     gameWon = false;
     gameOver = false;
     lifeLoss = false;
     levelDisplay.textContent = "Level: " + (level + 1);
+    titleDisplay.textContent = "Unscrambler";
+
     for(let cube of wordTable){
         cube.style.borderColor = "black";
     }
-    blankSpaces.textContent = "";
-    addClickListener()
-    answerDisplay();
     
     t1.textContent = levelsArray[level].board[0];
     t2.textContent = levelsArray[level].board[1];
@@ -63,67 +82,44 @@ function initGame(){
     l6.textContent = levelsArray[level].board[14];
     l7.textContent = levelsArray[level].board[15];
 
-
-
 }
-initGame();
-
-
-//*--- FUNCTIONS ---*//
-
 //checks for win
 function checkForWin(){    
-    if(lettersPlayed.length === 9 && joinedWordsSeperate.length !== levelsArray[level].levelAnswers.length){
+    if(lettersPlayed.length >= 9 && joinedWordsSeperate.length !== levelsArray[level].levelAnswers.length){
         lifeLoss = true;
-        console.log("Life lost. Lives left: " + (lives-=1));
+        lifeCount();
 
     }else{
         for(let i = 0; i < lettersPlayed.length; i++){
     
             if(levelsArray[level].levelAnswers.includes(lettersPlayed.join(''))){  
                 joinedWordsSeperate.push(this.lettersPlayed.join(''));
-                lettersPlayed = [];
+                for(let w = 0; w < blankSpaces.childElementCount; w++){
+                    if(this.lettersPlayed.join('').length === blankSpaces.childNodes[w].textContent.length){
+                        blankSpaces.childNodes[w].textContent = this.lettersPlayed.join('');
+                    }
+                }lettersPlayed = [];
             }
         }
         console.log(joinedWordsSeperate);
-        for(let z = 0; z < answerList.length; z++){
-            for(let i = 0; i < joinedWordsSeperate.length; i++){
-                if(answerList[z].length === joinedWordsSeperate[i].length){
-                    answerList[z].childNodes.textContent = joinedWordsSeperate[i];
-                }
-            }
-            }
+        
         if(joinedWordsSeperate.length === levelsArray[level].levelAnswers.length){
-        console.log("Level Complete!");
-        nextLevel();
+        titleDisplay.textContent = "Level Complete!";
+        setTimeout(nextLevel, 1500);
         }
     }
 }
 
 // displays answers as spaces
 function answerDisplay(){
+    blankSpaces.textContent = "";
     for(let word of levelsArray[level].levelAnswers){
         let wordArray = word.length;
-        let space = "_ "
+        let space = "_"
         let newLi = document.createElement('li');
         newLi.textContent = space.repeat(wordArray);
         blankSpaces.appendChild(newLi);
     }
-}
-
-//changes squares as they are clicked
-function letterPush(){
-    lettersPlayed.push(event.target.textContent);
-    event.target.style.borderColor = "orangeRed";
-    event.target.removeEventListener('click', letterPush);
-    checkForWin();
-}
-
-
-//function for hint button
-function hint(){
-    lives -= 1;
-    alert(levelsArray[level].hint);
 }
 
 // adding click function to all table elements
@@ -134,13 +130,47 @@ function addClickListener(){
     })
 }
 
-// initiates next level, changes board to next level
-function nextLevel(){
-    lives = 5;
-    level+=1;
-    lettersPlayed.length = 0;
-    joinedWordsSeperate.length = 0;
+//changes squares as they are clicked
+function letterPush(){
+    lettersPlayed.push(event.target.textContent);
+    event.target.style.borderColor = "orangeRed";
+    event.target.removeEventListener('click', letterPush);
+    checkForWin();
+    console.log(event.target.textContent);
+}
 
-    answerDisplay();
-    initGame();
+
+//life creator 
+function lifeCreator (){
+    for(let i = 0; i < 5; i++){
+        let eggs = document.createElement('img');
+        eggs.src = "./images/egg.png";
+        lives.push(eggs);
+        livesImages.appendChild(eggs);
+        }
+}
+
+//adjusts lives displayed on screen ** needs to remove image
+function lifeCount(){
+    if(lifeLoss === true){
+        lives.pop();
+        titleDisplay.textContent = ("Life Lost. " + lives.length + " left.");
+    }
+}
+
+//function for hint button
+function hint(){
+    lives.pop();
+    alert(levelsArray[level].hint);
+}
+
+
+
+
+//after all lives are lost, initiate this function
+
+function gameOverScreen(){
+    if(lives.length === 0){
+        titleDisplay.textContent = "Game Over"
+    };
 }
--- js/gameData.js
+++ js/gameData.js
@@ -1,21 +1,40 @@
-/*----- constants -----*/
-var startBtn;
-var wordBank = [
-    ["EGG", "CHEESE"], //1
-    ["SPOON","FORK"], //2
-    ["APPLE", "PEAR"], //3
-    ["BUTTER","JAM"], //4
-    ["COFFEE", "TEA"], //5
-    ["SNAKE","FISH"], //6
-    ["DOG","FERRET"], //7
-    ["CHICKEN","HORSE","GOAT"], //8
-    ["SHEEP","GOAT","PIG","HEN"], //9
-    ["TIGER","LION","PANTHER"], //10
-    ["EAGLE","HAWK","VULTURE"], //11
-    ["SHARK","DOLPHIN","SEAL"], //12
-    ["JAVASCRIPT","IS","NEAT"] //13
-]
+/*----- cached element references -----*/
+var wordTable = document.querySelectorAll('td');
+var eggs = document.querySelectorAll('img');
+var livesImages = document.getElementById('lives');
 
+var titleDisplay = document.getElementById('title-display');
+
+var blankSpaces = document.querySelector('ul');
+var answerList = document.querySelectorAll('li');
+var fiveLives = document.querySelectorAll('.eggs');
+var levelDisplay = document.getElementById('level-display');
+
+var hintBtn = document.getElementById('hint');  
+var retryBtn = document.getElementById('retry');
+hintBtn.addEventListener("click", hint);
+retryBtn.addEventListener("click", retryLevel);
+
+var t1 = document.getElementById('t1');
+var t2 = document.getElementById('t2');
+var t3 = document.getElementById('t3');
+var t4 = document.getElementById('t4');
+var t5 = document.getElementById('t5');
+var t6 = document.getElementById('t6');
+var t7 = document.getElementById('t7');
+var t8 = document.getElementById('t8');
+var t9 = document.getElementById('t9');
+var l1 = document.getElementById('l1');
+var l2 = document.getElementById('l2');
+var l3 = document.getElementById('l3');
+var l4 = document.getElementById('l4');
+var l5 = document.getElementById('l5');
+var l6 = document.getElementById('l6');
+var l7 = document.getElementById('l7');
+
+/*----- constants -----*/
+
+var startBtn;
 
 /*----- app's state (variables) -----*/
 
@@ -24,12 +43,13 @@ var gameOver;
 var level = 0;
 
 var lifeLoss;
-var lives;
+var lives = [];
 
 var lettersPlayed = [];
 var joinedWordsSeperate = [];
 
 /*----- Levels -----*/
+
 var levelsArray = [
          {board: ["C","G","E","H","E","G","E","S","E"],
          hint: "breakfast",
@@ -52,50 +72,83 @@ var levelsArray = [
          levelAnswers: ["COFFEE", "TEA"]
         } ,
         {board: ["H","S","I","F","S","N","A","K","E"],
-         hint: "animals",
+         hint: "pets",
          levelAnswers: ["SNAKE", "FISH"]
+        } ,
+        {board: ["D","O","G","F","E","R","R","E","T"],
+         hint: "pets",
+         levelAnswers: ["DOG", "FERRET"]
+        } ,
+        {board: ["C","H","I","C","K","E","N","H","O","R","S","E","G","O","A","T"],
+         hint: "farm animals",
+         levelAnswers: ["CHICKEN", "HORSE", "GOAT"],
         }
+]
+
+var wordBank = [
+    ["EGG", "CHEESE"], //1
+    ["SPOON","FORK"], //2
+    ["APPLE", "PEAR"], //3
+    ["BUTTER","JAM"], //4
+    ["COFFEE", "TEA"], //5
+    ["SNAKE","FISH"], //6
+    ["DOG","FERRET"], //7
+    ["CHICKEN","HORSE","GOAT"], //8
+    ["SHEEP","GOAT","PIG","HEN"], //9
+    ["TIGER","LION","PANTHER"], //10
+    ["EAGLE","HAWK","VULTURE"], //11
+    ["SHARK","DOLPHIN","SEAL"], //12
+    ["JAVASCRIPT","IS","NEAT"] //13
 ]
 
 /*----- functions -----*/
 
 
-//adjusts lives displayed on screen ** needs to remove image
-
-function lifeCount(){
-    if(lifeLoss === true){
-        lives -= 1;
-        console.log("Life Lost. Lives left: " + lives);
-    }
+//increases board size
+function levelUp(){
+    var extraTableElements = [l1,l2,l3,l4,l5,l6,l7];
+    extraTableElements.forEach(function(){
+        this.style.visibility = "visible";
+    })
 }
+
 
 // readjusts the board after retry level is clicked
 
 function retryLevel(){
      if(joinedWordsSeperate.length !== levelsArray[level].levelAnswers.length){
-        lettersPlayed.length = 0;
         joinedWordsSeperate.length = 0;
+        lettersPlayed.length = 0;
+        gameWon = false;
+        gameOver = false;
+        levelDisplay.textContent = "Level: " + (level + 1);
         for(let cube of wordTable){
-            cube.addEventListener('click',letterPush);
             cube.style.borderColor = "black";
         }
         blankSpaces.textContent = "";
+        addClickListener()
         answerDisplay();
+        
+        t1.textContent = levelsArray[level].board[0];
+        t2.textContent = levelsArray[level].board[1];
+        t3.textContent = levelsArray[level].board[2];
+        t4.textContent = levelsArray[level].board[3];
+        t5.textContent = levelsArray[level].board[4];
+        t6.textContent = levelsArray[level].board[5];
+        t7.textContent = levelsArray[level].board[6];
+        t8.textContent = levelsArray[level].board[7];
+        t9.textContent = levelsArray[level].board[8];
+    
+        l1.textContent = levelsArray[level].board[9];
+        l2.textContent = levelsArray[level].board[10];
+        l3.textContent = levelsArray[level].board[11];
+        l4.textContent = levelsArray[level].board[12];
+        l5.textContent = levelsArray[level].board[13];
+        l6.textContent = levelsArray[level].board[14];
+        l7.textContent = levelsArray[level].board[15];
+    
+    
 }
 }
 
-//after all lives are lost, initiate this function
 
-function gameOverScreen(){
-    if(lives === 0){
-        alert('gameOver');
-    };
-}
-
-//increases board size
-function levelUp(){
-    var tableElements = [t1,t2,t3,t4,t5,t6,t7,t8,t9,l1,l2,l3,l4,l5,l6,l7];
-    tableElements.forEach(function(){
-        style.visibility = "visible";
-    })
-}
